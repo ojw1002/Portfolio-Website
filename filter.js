d191536021be72ca7e0f9ac5f27e3d51ea0af81f
oliviawarner <ojw1002@sru.edu>
2020-11-23 16:42:39 -0500

    created php file for contact pg

---
+++ filter.js
@@ -0,0 +1,31 @@
+//Filters Portfolio Project by their data filter attribute
+//These attributes are defined within the html portfolio doc
+
+//JQuery
+
+$(document).ready(function(){
+
+    //assigning the data attribute
+    $(".filter-button").click(function(){
+    var value = $(this).attr('data-filter');
+    
+    //shows all gallery dates
+    if(value == "all"){
+      $('.filter').show('1000');
+    }
+  
+    //will hide the rest of the dates if it is not of the same value
+    else {
+      $(".filter").not('.'+ value).hide('3000');
+      $('.filter').filter('.'+ value).show('3000');
+    
+    }
+    });
+  
+    if ($(".filter-button").removeClass("active")) {
+      $(this).removeClass("active");
+    }
+    
+    $(this).addClass("active");
+
+});
